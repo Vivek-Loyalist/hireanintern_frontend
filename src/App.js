6d5b147ae045650fd68949e6db6140058ee6b9ff
VivekGurram <61747334+vivek-chintu@users.noreply.github.com>
2023-10-03 18:51:22 -0400

    Login pages added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,86 @@
 import './App.css';
+import {createBrowserRouter,RouterProvider,Route,Link,} from "react-router-dom";
 import LandindFooter from './components/LandingFooter';
 import LandingPage from './pages/LandingPage';
 import LandingHeader from './components/LandingHeader';
+import Intern_Login_Page from './pages/Intern_Login_Page';
+import Employer_Login_Page from './pages/Employer_Login_Page';
+import Employer_Signup_Page from './pages/Employer_Signup_Page';
+import Intern_Signup_Page from './pages/Intern_Signup_Page';
+import Intern_Landing_Page from './pages/Intern_Landing_Page';
+import Intern_Dashboard from './pages/Intern_Dashboard';
 
 
 
 function App() {
+
+  const router = createBrowserRouter([
+    
+    // Route for Landing Page
+    {
+      path: "/",
+      element: (
+        <LandingPage />
+      ),
+    },
+
+
+    // Route for Intern Login Page
+    {
+      path: "/login/intern",
+      element: (
+        <Intern_Login_Page />
+      ),
+    },
+
+
+    {
+      path: "/signup/intern",
+      element: (
+        <Intern_Signup_Page />
+      ),
+    },
+
+
+
+    // Route for Employer Login Page
+
+    {
+      path: "/login/employer",
+      element: (
+        <Employer_Login_Page />
+      ),
+    },
+
+    // Route for EMployer Signup Page
+    {
+      path: "/signup/employer",
+      element: (
+        <Employer_Signup_Page />
+      ),
+    },
+
+
+
+
+  ]);
+
+
   return (
     <div className="App">
 
       <LandingHeader />
-      <LandingPage />
+
+          <RouterProvider router={router} />   
+
+
+        {/* <Intern_Dashboard />  */}
+
+        {/* <Intern_Landing_Page />  */}
+      
+
       <LandindFooter />
-      {/* changed  */}
+     
       
 
     </div>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import BoxComp from "./Landing_Page_Content";
 import Landing_Page_Content from "./Landing_Page_Content";
 
 
