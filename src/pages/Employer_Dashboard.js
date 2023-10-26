1c408f21522f98134e98dabbbfca60fa4a88f878
VivekGurram <61747334+vivek-chintu@users.noreply.github.com>
2023-10-26 15:41:18 -0400

    Misc

--- src/pages/Employer_Dashboard.js
+++ src/pages/Employer_Dashboard.js
@@ -1,11 +1,78 @@
-import * as React from 'react';
+// import * as React from 'react';
+// import { styled } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
+// // import Grid from '@mui/material/Grid';
+// import Button from '@mui/material/Button';
+// import ButtonGroup from '@mui/material/ButtonGroup';
+// import { useNavigate } from "react-router-dom";
+// import videoSource from "../images/Indeed2.mp4";
+
+// const VideoBackground = styled(Box)(({ theme }) => ({
+//   position: 'relative',
+//   overflow: 'hidden',
+//   width: '100vw',
+//   height: '100vh',
+//   '& video': {
+//     objectFit: 'cover',
+//     width: '100%',
+//     height: '100%',
+//     position: 'fixed',
+//     top: '0',
+//     left: '0',
+//     zIndex: '-1',
+//   },
+// }));
+
+// const ContentBox = styled(Box)(({ theme }) => ({
+//   display: 'flex',
+//   alignItems: 'center',
+//   justifyContent: 'center',
+//   backgroundColor: 'rgba(0, 0, 0, 0.5)', // You can adjust the background color's transparency
+//   height: '100%',
+//   color: '#fff',
+// }));
+
+// export default function Employer_Dashboard() {
+//   const navigate = useNavigate();
+
+//   return (
+//     <div>
+//       {/* <h1>Employer Dashboard</h1> */}
+
+//       <VideoBackground>
+//         <video autoPlay loop muted>
+//           <source src={videoSource} type="video/mp4" />
+//         </video>
+//         <ContentBox>
+//           <div>
+//             <h1>Let's make your next great hire. Fast</h1>
+//             <h5>Save time and effort in your hiring journey. Finding the best fit for the job shouldn’t be a full-time job. Indeed’s simple and powerful tools let you source, screen, and hire faster.</h5>
+//             <ButtonGroup
+//               disableElevation
+//               variant="contained"
+//               aria-label="Disabled elevation buttons"
+//               style={{ display: "flex", justifyContent: "center" }}
+//             >
+//               <Button style={{ margin: 10 }} onClick={() => navigate('/employer/postjob')}>Post A Job</Button>
+//               <Button style={{ margin: 10 }}>Find Applicants</Button>
+//             </ButtonGroup>
+//           </div>
+//         </ContentBox>
+//       </VideoBackground>
+//     </div>
+//   );
+// }
+
+
+
+import React, { useEffect, useState } from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-// import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import videoSource from "../images/Indeed2.mp4";
+import axios from 'axios';
 
 const VideoBackground = styled(Box)(({ theme }) => ({
   position: 'relative',
@@ -27,18 +94,33 @@ const ContentBox = styled(Box)(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  backgroundColor: 'rgba(0, 0, 0, 0.5)', // You can adjust the background color's transparency
+  backgroundColor: 'rgba(0, 0, 0, 0.5)',
   height: '100%',
   color: '#fff',
 }));
 
 export default function Employer_Dashboard() {
   const navigate = useNavigate();
+  const { email } = useParams();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:4000/job/company_name/${email}`);
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (email) {
+      fetchData();
+    }
+  }, [email]);
 
   return (
     <div>
-      <h1>Employer Dashboard</h1>
-
       <VideoBackground>
         <video autoPlay loop muted>
           <source src={videoSource} type="video/mp4" />
@@ -56,6 +138,14 @@ export default function Employer_Dashboard() {
               <Button style={{ margin: 10 }} onClick={() => navigate('/employer/postjob')}>Post A Job</Button>
               <Button style={{ margin: 10 }}>Find Applicants</Button>
             </ButtonGroup>
+            {data && (
+              <div>
+                <h2>Welcome to the Employer Dashboard</h2>
+                <p>Email: {email}</p>
+                <p>Company Name: {data.companyName}</p>
+                {/* ... other dashboard content */}
+              </div>
+            )}
           </div>
         </ContentBox>
       </VideoBackground>
--- src/pages/Employer_Login_Page.js
+++ src/pages/Employer_Login_Page.js
@@ -1,3 +1,187 @@
+// import * as React from 'react';
+// import Avatar from '@mui/material/Avatar';
+// import Button from '@mui/material/Button';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+// import Link from '@mui/material/Link';
+// import Paper from '@mui/material/Paper';
+// import Box from '@mui/material/Box';
+// import Grid from '@mui/material/Grid';
+// import Typography from '@mui/material/Typography';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import { useNavigate } from "react-router-dom";
+// import axios from 'axios';
+// import Snackbar from '@mui/material/Snackbar';
+// import Alert from '@mui/material/Alert';
+// import { baseURL } from '../baseURL';
+
+
+// // TODO remove, this demo shouldn't need to reset the theme.
+
+// const defaultTheme = createTheme();
+
+// function SignInSide() {
+
+//   const [email, setEmail] = React.useState("");
+//   const [password, setPassword] = React.useState("");
+//   const [errorMsg, setErrorMsg] = React.useState(false);
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const data = new FormData(event.currentTarget);
+//     console.log({
+//       email: data.get('email'),
+//       password: data.get('password'),
+//     });
+//   };
+
+//   const navigate = useNavigate();
+
+
+//   // // const navigate = useNavigate();
+
+//   const onSignin = async () => {
+//     //Write authentication code here
+//     //  navigate("/employer/dashboard");
+//     const data = {
+//       email: email,
+//       password: password
+//     }
+//     try {
+//       const response = await axios.post(`${baseURL}/employer/login`, data);
+//       if (response.status === 200) {
+//         navigate("/employer/dashboard");
+//       }
+//     } catch (error) {
+//       setErrorMsg(true);
+//       console.error(error);
+//     }
+//     // window.location.href="/employer/dashboard";
+
+//   }
+
+
+//   return (
+//     <>
+//     <ThemeProvider theme={defaultTheme}>
+//       <Grid container component="main" sx={{ height: '100vh' }}>
+//         <CssBaseline />
+//         <Grid
+//           item
+//           xs={false}
+//           sm={4}
+//           md={7}
+//           sx={{
+//             backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+//             // backgroundRepeat: 'no-repeat',
+
+
+//             backgroundColor: (t) =>
+//               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+//             backgroundSize: 'cover',
+//             backgroundPosition: 'center',
+//           }}
+//         />
+//         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+//           <Box
+//             sx={{
+//               my: 8,
+//               mx: 4,
+//               display: 'flex',
+//               flexDirection: 'column',
+//               alignItems: 'center',
+//             }}
+//           >
+//             <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+
+//             </Avatar>
+//             <Typography component="h1" variant="h5">
+//               Employer Sign in
+//             </Typography>
+//             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+//               <TextField
+//                 margin="normal"
+//                 required
+//                 fullWidth
+//                 id="email"
+//                 label="Email Address"
+//                 name="email"
+//                 autoComplete="email"
+//                 value={email}
+//                 onChange={(e) => setEmail(e.target.value)}
+//                 autoFocus
+//               />
+//               <TextField
+//                 margin="normal"
+//                 required
+//                 fullWidth
+//                 name="password"
+//                 label="Password"
+//                 type="password"
+//                 id="password"
+//                 value={password}
+//                 onChange={(e) => setPassword(e.target.value)}
+//                 autoComplete="current-password"
+//               />
+//               <FormControlLabel
+//                 control={<Checkbox value="remember" color="primary" />}
+//                 label="Remember me"
+//               />
+
+
+//               <Button
+//                 onClick={() => onSignin()}
+//                 fullWidth
+//                 variant="contained"
+//                 sx={{ mt: 3, mb: 2 }}
+//               >
+//                 Sign In
+//               </Button>
+
+
+//               <Grid container>
+//                 <Grid item xs>
+//                   <Link href="http://localhost:3000/" variant="body2">
+//                     Forgot password?
+//                   </Link>
+//                 </Grid>
+//                 <Grid item>
+
+//                   {/* Button for signup/employer */}
+
+//                   <Link href="/signup/employer" variant="body2" >Don't Have Account? SignUp</Link>
+
+
+//                 </Grid>
+//               </Grid>
+//             </Box>
+//           </Box>
+//         </Grid>
+//       </Grid>
+//     </ThemeProvider>
+//       <Snackbar open={errorMsg} autoHideDuration={6000}>
+//         <Alert severity="error" sx={{ width: '100%' }}>
+//           Login Unsuccessful Check your details!
+//         </Alert>
+//       </Snackbar>
+//       </>
+//   );
+// }
+
+
+// export default function Employer_Login_Page(props) {
+//   return (
+//     <div className='App'>
+//       <SignInSide />
+//     </div>
+//   )
+// }
+
+
+
+
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
@@ -11,174 +195,142 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom'; // Updated import
 import axios from 'axios';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
 import { baseURL } from '../baseURL';
 
-
-// TODO remove, this demo shouldn't need to reset the theme.
-
 const defaultTheme = createTheme();
 
 function SignInSide() {
-
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
   const [errorMsg, setErrorMsg] = React.useState(false);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
-
   const navigate = useNavigate();
 
-
-  // // const navigate = useNavigate();
-
   const onSignin = async () => {
-    //Write authentication code here
-    //  navigate("/employer/dashboard");
     const data = {
       email: email,
-      password: password
-    }
+      password: password,
+    };
+
     try {
       const response = await axios.post(`${baseURL}/employer/login`, data);
       if (response.status === 200) {
-        navigate("/employer/dashboard");
+        navigate('/employer/dashboard'); // Pass email as a route parameter
+
+        console.log(response.data.email);
       }
     } catch (error) {
       setErrorMsg(true);
       console.error(error);
     }
-    // window.location.href="/employer/dashboard";
-
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <>
-    <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
-            // backgroundRepeat: 'no-repeat',
-
-
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
+      <ThemeProvider theme={defaultTheme}>
+        <Grid container component="main" sx={{ height: '100vh' }}>
+          <CssBaseline />
+          <Grid
+            item
+            xs={false}
+            sm={4}
+            md={7}
             sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+              backgroundColor: (t) =>
+                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
             }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Employer Sign in
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-
-
-              <Button
-                onClick={() => onSignin()}
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-
-
-              <Grid container>
-                <Grid item xs>
-                  <Link href="http://localhost:3000/" variant="body2">
-                    Forgot password?
-                  </Link>
+          />
+          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}></Avatar>
+              <Typography component="h1" variant="h5">
+                Employer Sign in
+              </Typography>
+              <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  onClick={() => onSignin()}
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="http://localhost:3000/" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="/signup/employer" variant="body2">
+                      Don't Have Account? Sign Up
+                    </Link>
+                  </Grid>
                 </Grid>
-                <Grid item>
-
-                  {/* Button for signup/employer */}
-
-                  <Link href="/signup/employer" variant="body2" >Don't Have Account? SignUp</Link>
-
-
-                </Grid>
-              </Grid>
+              </Box>
             </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </ThemeProvider>
+      </ThemeProvider>
       <Snackbar open={errorMsg} autoHideDuration={6000}>
         <Alert severity="error" sx={{ width: '100%' }}>
-          Login Unsuccessful Check your details!
+          Login Unsuccessful. Check your details!
         </Alert>
       </Snackbar>
-      </>
+    </>
   );
 }
 
-
 export default function Employer_Login_Page(props) {
   return (
-    <div className='App'>
+    <div className="App">
       <SignInSide />
     </div>
-  )
+  );
 }
-
-
-
-
